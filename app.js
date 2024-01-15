83266e063e691ae919f09ad690e04cf446abb4bb
Paolo Gomez <peweegardner248@gmail.com>
2024-01-15 07:18:57 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const guests = {
+    ANTONY: {
+      title: "General",
+      region: "Rome",
+      dietaryPreference: "Vegetarian",
+      pastGifts: ["Golden Laurel", "Chariot"]
+    },
+    CICERO: {
+      title: "Orator",
+      region: "Arpinum",
+      dietaryPreference: "Omnivore",
+      pastGifts: ["Scroll of Proverbs", "Quill"]
+    }
+  };
+  
