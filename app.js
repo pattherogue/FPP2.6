0206e77595cdd5439cb7e7b6a695f5b4798d44ac
Paolo Gomez <peweegardner248@gmail.com>
2024-01-15 10:52:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -12,4 +12,13 @@ const guests = {
       pastGifts: ["Scroll of Proverbs", "Quill"]
     }
   };
+
+  //Add "BRUTUS" to the guest directory. He's a "Senator" from "Rome", prefers "Vegan" food, and in the past, he has gifted Caesar a "Silver Dagger" and a "Marble Bust".
+  guests.BRUTUS = {
+    title: "Senator",
+    region: "Rome",
+    dietaryPreference: "Vegan",
+    pastGifts: ["Silver Dagger", "Marble Bust"]
+  };
   
+  //Update CICERO's past gifts to include a "Golden Lyre".
